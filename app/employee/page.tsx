121ec398a5385bb42abea5f2d3aeb92ca87788e4
Bernard <bdnterful@gmail.com>
2023-11-07 10:37:48 +0000

    resolved taskPage conflict

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -14,10 +14,22 @@ const Page = () => {
   return (
     <>
       <title>Employee | Home</title>
-      <div>Employee page</div>
-      <button type="button" onClick={() => signOut()}>
-        Signout
-      </button>
+      <TaskPage />
+      <div className="flex justify-center">
+      <form className="w-95 max-w-25rem mx-auto my-8" >
+      <div className="mb-2">
+        <label className="font-bold mb-2 text-#353336 block" htmlFor='new-password'>New Password</label>
+        <input className="block w-full p-1 bg-f7f0fa rounded-4 border border-#38015c" type='password' id='new-password' />
+      </div>
+      <div className="mb-2">
+        <label className="font-bold mb-2 text-#353336 block" htmlFor='old-password'>Old Password</label>
+        <input className="block w-full p-1 bg-f7f0fa rounded-4 border border-#38015c" type='password' id='old-password' />
+      </div>
+      <div className="mb-1">
+        <button className="cursor-pointer py-2 px-6 rounded-4 bg-#0d0c0d text-black border border-#0d0c0d">Change Password</button>
+      </div>
+    </form>
+      </div>
     </>
   );
 };
